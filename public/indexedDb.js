0a616baebb31e78b8d4882be68727894ce254106
Ariel Marchand <marchand.aa@gmail.com>
2021-07-25 12:48:36 -0600

    removed hidden characters from indexedDB

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -4,40 +4,40 @@ const indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
-​
+
 let db;
 const request = indexedDB.open("budget", 1);
-​
+
 request.onupgradeneeded = ({ target }) => {
   let db = target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
-​
+
 request.onsuccess = ({ target }) => {
   db = target.result;
-​
+
   // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
 };
-​
+
 request.onerror = function(event) {
   console.log("Oh no! " + event.target.errorCode);
 };
-​
+
 function saveRecord(record) {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
-​
+
   store.add(record);
 }
-​
+
 function checkDatabase() {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
   const getAll = store.getAll();
-​
+
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
@@ -60,6 +60,6 @@ function checkDatabase() {
     }
   };
 }
-​
+
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
