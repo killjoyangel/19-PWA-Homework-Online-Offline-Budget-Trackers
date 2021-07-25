11019cec008c50919d6ff1f38e225096e1090cd8
Ariel Marchand <marchand.aa@gmail.com>
2021-07-24 21:50:29 -0600

    update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/nameless-escarpment',
+  process.env.MONGODB_URI || 'mongodb://localhost/OfflineDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
