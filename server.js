71a4ea47bccf261112a2cd045d6aacba4f79e0b7
Ariel Marchand <marchand.aa@gmail.com>
2021-07-24 22:08:44 -0600

    updated mongose uri again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/OfflineDB',
+  process.env.MONGODB_URI || 'mongodb://localhost/OfflineBT',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
