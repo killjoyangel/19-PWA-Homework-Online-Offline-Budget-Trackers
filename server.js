461168b9e53472d40abeb337452c13797660a9ad
Ariel Marchand <marchand.aa@gmail.com>
2021-07-24 22:39:58 -0600

    adding a console log

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
@@ -22,7 +23,7 @@ mongoose.connect(
     useFindAndModify: false,
   },
 );
-
+console.log('testing')
 // routes
 app.use(require("./routes/api.js"));
 
