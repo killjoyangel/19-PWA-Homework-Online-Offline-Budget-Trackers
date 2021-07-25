891f6c7e2013e5d1515bb7555d607d418d9c0cc8
Ariel Marchand <marchand.aa@gmail.com>
2021-07-25 00:16:00 -0600

    updated server

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(logger("dev"));
 
